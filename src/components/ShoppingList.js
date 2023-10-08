e5a1c53f7c0408d77470899ce2f1525d2ef9a542
sofianek9 <kayasofiane7@gmail.com>
2023-10-08 13:42:39 +0200

    V2

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,32 +1,29 @@
 import React from "react";
-import '../components/styles/ShoopinList.css'
+import "../components/styles/ShoopinList.css";
 import { plantList } from "../data/plantList";
 
 const ShoppingList = () => {
-
   const categories = plantList.reduce(
     (acc, plant) =>
       acc.includes(plant.category) ? acc : acc.concat(plant.category),
     []
   );
 
-  
-
   return (
     <div>
-
       <ul>
         {categories.map((cat) => (
-          <li key={cat}>{cat}</li>
+          <li>{cat}</li>
         ))}
       </ul>
 
       <ul className="lmj-plant-list">
-        {plantList.map((plant) => (
+        {plantList.map((plant, index) => (
           <li key={plant.id} className="lmj-plant-item">
-            {plant.name} 
-            {plant.isBestSale && <span>🔥</span>}
-            {plant.isSpecialOffer && <div className="lmj-sales">soldes</div>}
+            {plant.name}
+            {plant.isSpecialOffer ? (
+              <div className="lmj-sales">Soldes</div>
+            ) : null}
           </li>
         ))}
       </ul>
